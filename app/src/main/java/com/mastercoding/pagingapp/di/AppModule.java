378882a98fd63f576f9c778bfca647451b776891
DELL <omnianabil1920@gmail.com>
2024-07-17 15:22:11 +0300

    create paging source

---
+++ app/src/main/java/com/mastercoding/pagingapp/di/AppModule.java
@@ -0,0 +1,13 @@
+package com.mastercoding.pagingapp.di;
+
+import javax.inject.Singleton;
+
+import dagger.Module;
+import dagger.hilt.InstallIn;
+import dagger.hilt.components.SingletonComponent;
+
+
+@Module
+@InstallIn(SingletonComponent.class)
+public class AppModule {
+}
